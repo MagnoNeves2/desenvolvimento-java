9df0d339f8744ea48fa2ea77ef1897a7bd7473f4
Magno <magnoneves2@gmail.com>
2020-05-26 22:04:03 -0300

    Resolução da atividade de programação orientada a objetos Herança/Encapsulamento

---
+++ ExerciciosPooJava/src/veiculos/main.java
@@ -0,0 +1,34 @@
+package veiculos;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro();
+		Moto moto = new Moto();
+		
+		carro.setCor("Verde");
+		carro.setDono("Pedrinho");
+		carro.setMarca("Audi");
+		carro.setMotor(true);
+		carro.setPortas(4);
+		carro.setTipo("4x4");
+		carro.setVelocidadeMax(200);
+		
+		moto.setCor("Vermelha");
+		moto.setDono("Zezinho");
+		moto.setEstilo("Bobber");
+		moto.setMarca("Triumph");
+		moto.setPortas(0);
+		moto.setVelocidadeMax(150);
+		
+		carro.compra();
+		moto.vender();
+		
+		carro.decisao();
+		
+		
+		
+	}
+
+}
